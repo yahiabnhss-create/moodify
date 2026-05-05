36014b09ca94faf82fe39fba43a94f7321b26ddc
Yahia <yahia.bnhss@hes.so.ch>
2026-05-05 12:37:09 +0200

    fix: ajout scopes playlist-read-public et playlist-read-collaborative

--- src/services/spotify.js
+++ src/services/spotify.js
@@ -34,6 +34,8 @@ export async function loginWithSpotify() {
       'user-read-private',
       'user-read-playback-state',
       'user-modify-playback-state',
+      'playlist-read-public',
+      'playlist-read-collaborative',
     ].join(' '),
     code_challenge_method: 'S256',
     code_challenge: challenge,
