f0cf6fbd1d0997bf7408e90bce0a0c5a56f62de4
Yahia <yahia.bnhss@hes.so.ch>
2026-05-15 12:39:54 +0200

    fix: playlists Spotify par émotion (Mood Booster, Dance Hits, Peaceful Piano, Beast Mode, Lofi Girl)

--- src/constants/emotions.js
+++ src/constants/emotions.js
@@ -1,27 +1,27 @@
 export const EMOTIONS = {
   happy: {
     label: 'Heureux 😄',
-    genre: 'pop',
-    playlistId: '37i9dQZF1DXdPec7aLTmlC',
+    genre: 'pop / feel-good',
+    playlistId: '37i9dQZF1DX3rxVfibe1L0', // Mood Booster
   },
   surprised: {
     label: 'Surpris 😲',
-    genre: 'électro',
-    playlistId: '37i9dQZF1DX4dyzvuaRJ0n',
+    genre: 'dance / électro',
+    playlistId: '37i9dQZF1DX0BcQWzuB7ZO', // Dance Hits
   },
   sad: {
     label: 'Triste 😢',
-    genre: 'mélancolique',
-    playlistId: '37i9dQZF1DX3YSRoSdA634',
+    genre: 'piano / mélancolique',
+    playlistId: '37i9dQZF1DX4sWSpwq3LiO', // Peaceful Piano
   },
   angry: {
     label: 'En colère 😠',
-    genre: 'énergie',
-    playlistId: '37i9dQZF1DWTggY0yqBxES',
+    genre: 'workout / intense',
+    playlistId: '37i9dQZF1DX76Wlfdnj7AP', // Beast Mode
   },
   neutral: {
     label: 'Neutre 😐',
-    genre: 'rap fr',
-    playlistId: '37i9dQZF1DX2A29LI7xHn1',
+    genre: 'lofi / chill',
+    playlistId: '0vvXsWCC9xrXsKd4FyS8kM', // Lofi Girl
   },
 }
